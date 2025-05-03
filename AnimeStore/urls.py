2c08fbc3fd49475df85e9af23e6cf7d74f74bf21
Артур Тимчук <artur.tymchuk@gmail.com>
2025-05-03 21:15:17 +0300

    Create project apps and create home.html

--- AnimeStore/urls.py
+++ AnimeStore/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.contrib import admin
+from . import settings
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),  # Main page and static views
+    path('accounts/', include('accounts.urls')),
+    path('products/', include('products.urls')),
+    path('cart/', include('cart.urls')),
+    path('orders/', include('orders.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
